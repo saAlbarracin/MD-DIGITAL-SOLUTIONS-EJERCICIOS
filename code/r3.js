3e8f54e0983a701ceaff5205e927db909e5e5da1
Santiago <albarracinsantiago24@gmail.com>
2021-05-22 02:53:11 -0300

    Agregando Comentarios

--- code/r3.js
+++ code/r3.js
@@ -1,4 +1,4 @@
-function openModal() {
+function openModal() { // Una función que me permite, ejecutar el modal, y de esta manera mostrar el video.
     console.log('---')
     $("#myModal").modal('show');
 }
